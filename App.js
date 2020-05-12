e3eceaa8f07025783b7cc0cf0ed46594bc22b11f
Danny Armanta <adannya@hotmail.com>
2020-05-12 12:27:53 +0700

    belajar contentment React native untuk 1. text 2. Image 3. TextInput 4. View

--- App.js
+++ App.js
@@ -8,107 +8,57 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  Image,
+  TextInput
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step ASOE</Text>
-              <Text style={styles.sectionDescription}>
-                Lek arep ngedit, bukaen <Text style={styles.highlight}>App.js</Text> trus rubahen file iki, 
-                engkok muncul e nang android yo podo karo iki.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View>
+        <View style = {
+          {
+            width: 80,
+            height: 80,
+            backgroundColor: '#48dbfb'
+          }
+        }
+        />     
+        <Text> Alfian </Text> 
+        <Alfian />
+        <Text> Danny </Text> 
+        <Foto />
+        <TextInput style = {
+            {
+              borderWidth: 1
+            }
+          }
+        /> 
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const Alfian = () => {
+  return <Text> S1 Sistem Informasi </Text>;
+};
+
+const Foto = () => {
+  return ( 
+    <Image source = {
+      {
+        uri: 'http://placeimg.com/100/100/arch'
+      }
+    }
+    style = {
+      {
+        width: 100,
+        height: 100
+      }
+    }
+    />
+  );
+};
 
 export default App;
