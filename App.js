f76b54eb3c6c750a5ecf3c2b4d1bcd5b7cf5629a
Danny Armanta <adannya@hotmail.com>
2020-05-13 13:47:56 +0700

    pembuatan fungsi ECOm

--- App.js
+++ App.js
@@ -11,43 +11,106 @@ import {
   Text,
   View,
   Image,
-  TextInput
+  TextInput,
+  StyleSheet
 } from 'react-native';
+import ROG from './img/produk1.png'
 
+const SampleBaru = () => {
+  return ( 
+    <View>
+    <View style = {
+      {
+        width: 80,
+        height: 80,
+        backgroundColor: '#48dbfb'
+      }
+    }
+    />      
+    <Text style={styles.text}> Alfian </Text>  
+    <Alfian />
+    <Text style={styles.text}> Danny </Text>  
+    <Foto />
+    <TextInput style = {
+      {
+        borderWidth: 1
+      }
+    }
+
+    />  
+    <BoxGreen />
+    <Hewan />
+    </View>
+  );
+}
 
 const App = () => {
   return (
-    <View>
-        <View style = { 
-          {
-            width: 80,
-            height: 80,
-            backgroundColor: '#48dbfb'
-          }
-        }
-        />     
-        <Text> Alfian </Text> 
-        <Alfian />
-        <Text> Danny </Text> 
-        <Foto />
-        <TextInput style = {
-            {
-              borderWidth: 1
-            }
-          }
-        
-        /> 
-       <BoxGreen />
-       <Hewan />
+     <View>
+      {/* <SampleBaru />
+      <StylingBaru /> */}
+      <ECom />
+      <ECom />
+      <ECom />
+     </View>
+
+  ); 
+};
+
+const ECom = () => {
+  return (
+    <View 
+    style = {{
+      padding: 15, 
+      backgroundColor : '#F2F2F2',
+      width : 212,
+      borderRadius : 8,
+      margin : 20,
+      }}>
+      <Image 
+        source = {ROG}
+        style = {{
+          width : 188,
+          height : 107,
+          borderRadius : 8,
+        }}
+        />
+      <Text style ={{fontSize : 14, fontWeight : 'bold', marginTop : 16}}>
+        ROG Terbaru 2030
+      </Text>
+      <Text style ={{fontSize : 12, fontWeight : 'bold',color : '#F2994A', marginTop : 12}}>
+        Rp. Cari Sendiri Di Google
+      </Text>
+      <Text style ={{fontSize : 12, fontWeight : '300', marginTop : 12}}>
+        Bukan Di Indonesia
+      </Text>
     </View>
   );
-};
+}
 
 // penggunaan component biasa
 const Alfian = () => {
-  return <Text> S1 Sistem Informasi </Text>;
+  return <Text style={styles.text}> S1 Sistem Informasi </Text>;
 };
 
+const StylingBaru = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View 
+        style = {{
+          width : 100,
+          height : 100,
+          backgroundColor : '#0abde3',
+          borderWidth : 2,
+          borderColor : '#6f27cd',
+          marginTop : 20,
+          marginLeft : 20,
+        }}
+      />
+    </View>
+  );
+}
 const Foto = () => {
   return ( 
     <Image source = {
@@ -70,7 +133,7 @@ const Foto = () => {
 
 class BoxGreen extends Component {
   render(){
-    return <Text>Ini Component Dari Class</Text>
+    return <Text style={styles.text}>Ini Component Dari Class</Text>
   }
 }
 
@@ -80,7 +143,7 @@ class Hewan extends Component {
       <View>
         <Image 
         source= {{uri:'http://placeimg.com/100/100/animals'}}
-        style = {{width: 100, height: 100, borderRadius: 50}}
+        style = {{width: 100, height: 100,marginLeft : 20, borderRadius: 50}}
         />
         <Text style = {{fontSize : 24, color : 'blue'}} >ini foto hewan</Text>
       </View>
@@ -88,5 +151,15 @@ class Hewan extends Component {
   }
 }
 
+//styling basic
+const styles = StyleSheet.create ({
+  text : {
+    fontSize : 18,
+    fontWeight : 'bold',
+    color : '#10ac84',
+    marginLeft : 20,
+    marginTop : 40,
+  }
+});
 
 export default App;
