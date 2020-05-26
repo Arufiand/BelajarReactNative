b68363dbb203ed756d153ce4fbe4c1af1cfbada8
arufiand <alfiandannyarmanta@gmail.com>
2020-05-26 18:15:07 +0700

    Belajar membuat koneksi dengan state dan props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   Text,
@@ -24,6 +16,7 @@ import FlexBox from './pages/FlexBox';
 import Position from './pages/Position';
 import PropsDinamis from './pages/PropsDinamis';
 import StateDinamis from './pages/StateDinamis';
+import Komunikasi from './pages/Komunikasi';
 
 const App = () => {
   return (
@@ -35,47 +28,13 @@ const App = () => {
           <Halamanlogin />
           <PropsDinamis />
           <FlexBox />
-          <SampleBaru />     
+          {/* <SampleBaru />      */}
           {/* <Styling /> */}
-          <ECom text1="ASUS 2020" text2="Rp2000000" text3="Dijual di ID" lebar = {250} />
-          <ECom text1 = "ASUS 2020" text2 = "Rp2000000" text3 = "Dijual di ID" lebar = {280}/ >
-          <ECom text1 = "ASUS 2020" text2 = "Rp2000000" text3 = "Dijual di ID" lebar = {320}/ >
           <StateDinamis />
+          <Komunikasi />
       </ScrollView>
      </View>
   ); 
 };
-
-{/* props ini digunakan untuk mengganti variabel */}
-const ECom = props => {
-  return (
-    <View 
-    style = {{
-      padding: 15, 
-      backgroundColor : '#F2F2F2',
-      width : props.lebar,
-      borderRadius : 8,
-      margin : 20,
-      }}>
-      <Image 
-        source = {ROG}
-        style = {{
-          width : 188,
-          height : 107,
-          borderRadius : 8,
-        }}
-        />
-      <Text style ={{fontSize : 14, fontWeight : 'bold', marginTop : 16}}>
-        {props.text1}
-      </Text>
-      <Text style ={{fontSize : 12, fontWeight : 'bold',color : '#F2994A', marginTop : 12}}>
-        {props.text2}
-      </Text>
-      <Text style ={{fontSize : 12, fontWeight : '300', marginTop : 12}}>
-        {props.text3}
-      </Text>
-    </View>
-  );
-}
 
 export default App;
